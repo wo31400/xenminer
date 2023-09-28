cede6d2799c53bfb47bbabb2592f4997c31c9e27
yuan <yuansir94@gmail.com>
2023-09-29 03:04:17 +0800

    1

--- start.sh
+++ start.sh
@@ -6,7 +6,6 @@ N=31  # 设置要执行的次数
 for ((i=1; i<=N; i++))
 do
     logfile="miner_$i.log"
-    pidfile="miner_$i.pid"
-    nohup python3 /home/xenminer/miner.py >> "$logfile" 2>&1 & echo $! > "$pidfile"
-    echo "miner $i 线程执行，日志文件：$logfile，PID 文件：$pidfile"
+    nohup python3 /home/xenminer/miner.py >> "$logfile" 2>&1 &
+    echo "miner $i 线程执行，日志文件：$logfile"
 done
